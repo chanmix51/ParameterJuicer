05c489d037e6ce8742119df38605e9aea07d481b
Grégoire HUBERT <hubert.greg@gmail.com>
2017-02-07 21:12:09 +0100

    add a working version of Juicer

---
+++ sources/lib/ParameterJuicer.php
@@ -0,0 +1,240 @@
+<?php
+/*
+ * This file is part of Chanmix51’s ParameterJuicer package.
+ *
+ * (c) 2017 Grégoire HUBERT <hubert.greg@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Chanmix51\ParameterJuicer;
+
+use Chanmix51\ParameterJuicer\ValidationException;
+
+/**
+ * ParameterJuicer
+ *
+ * @package   ParameterJuicer
+ * @copyright 2017 Grégoire HUBERT
+ * @author    Grégoire HUBERT <hubert.greg@gmail.com>
+ * @license   X11 {@link http://opensource.org/licenses/mit-license.php}
+ */
+class ParameterJuicer
+{
+    const STRATEGY_IGNORE_EXTRA_VALUES = 0;
+    const STRATEGY_REFUSE_EXTRA_VALUES = 1;
+    const STRATEGY_ACCEPT_EXTRA_VALUES = 2;
+
+    /** @var  array     list of validators, must be callables */
+    protected $validators   = [];
+
+    /** @var  array     list of cleaners, must be callables */
+    protected $cleaners     = [];
+
+    /** @var  array     list of fields, this gives an information if the field
+                        is mandatory or optional. */
+    protected $fields       = [];
+
+    /**
+     * addField
+     *
+     * Declare a new field with no validators nor cleaner. It can be declared
+     * if the field is optional or mandatory.
+     * If the field already exists, it is overriden.
+     */
+    public function addField(string $name, bool $is_mandatory = true): self
+    {
+        $this->fields[$name] = $is_mandatory;
+
+        return $this;
+    }
+
+    /**
+     * addFields
+     *
+     * Declare several fields at once.Existing fields are overriden.
+     */
+    public function addFields(array $fields, $are_mandatory = true): self
+    {
+        array_merge($this->fields, array_fill_keys($fields, $are_mandatory));
+
+        return $this;
+    }
+
+    /**
+     * addValidator
+     *
+     * Add a new validator associated to a key. If the field is not already declared, it is created.
+     */
+    public function addValidator(string $name, callable $validator): self
+    {
+        $this
+            ->checkFieldExists($name)
+            ->validators[$name][]  = $validator
+            ;
+
+        return $this;
+    }
+
+    /**
+     * addCleaner
+     *
+     * Add a new cleaner associated to a key.
+     */
+    public function addCleaner($name, callable $cleaner): self
+    {
+        $this
+            ->checkFieldExists($name)
+            ->cleaners[$name][] = $cleaner
+            ;
+
+        return $this;
+    }
+
+    /**
+     * validateAndClean
+     *
+     * Validate and clean the given data according to the definition.
+     */
+    public function validateAndClean(array $values, $strategy = 0): array
+    {
+        $exception = new ValidationException;
+
+        if ($strategy <> self::STRATEGY_ACCEPT_EXTRA_VALUES) {
+            $values = $this->applyStrategy($exception, $values, $strategy);
+        } 
+
+        return $this
+            ->validate($values, $exception)
+            ->clean($values)
+            ;
+    }
+
+    /**
+     * validate
+     *
+     * Trigger validation on values.
+     */
+    protected function validate(array $values, ValidationException $exception = null): self
+    {
+        $exception = isset($exception)
+            ? $exception
+            : new ValidationException
+            ;
+
+        foreach ($this->fields as $field => $is_mandatory) {
+            $is_set = isset($values[$field]);
+
+            if ($is_mandatory && !$is_set) {
+                $exception->addMessage(sprintf("Missing field '%s' is mandatory.", $field));
+
+                continue;
+            }
+
+            if ($is_set && isset($this->validators[$field])) {
+                foreach ($this->validators[$field] as $validator) {
+                    try {
+                        if (call_user_func($validator, $values[$field]) === false) {
+                            throw new \RuntimeException(
+                                sprintf("One of the validators for the field '%s' has a PHP error.", $field)
+                            );
+                        }
+                    } catch (ValidationException $e) {
+                        $exception->addMessage($e->getMessage());
+                    }
+                }
+            }
+        }
+
+        if ($exception->hasMessages()) {
+            throw $exception;
+        }
+
+        return $this;
+    }
+
+    /**
+     * clean
+     *
+     * Clean and return values.
+     */
+    protected function clean(array $values): array
+    {
+        foreach ($values as $field_name => $value) {
+            if (isset($this->cleaners[$field_name])) {
+                foreach ($this->cleaners[$field_name] as $cleaner) {
+                    $values[$field_name] = call_user_func($cleaner, $value);
+                }
+            }
+        }
+
+        return $values;
+    }
+
+    /**
+     * applyStrategy
+     *
+     * Apply extra values strategies.
+     *
+     * @throws RuntimeException if no valid stratgies were provided.
+     */
+    private function applyStrategy(ValidationException $exception, array $values, int $strategy): array
+    {
+        $diff_keys = array_keys(
+            array_diff_key(
+                $values,
+                $this->fields
+            )
+        );
+
+        if (count($diff_keys) === 0 || $strategy === self::STRATEGY_ACCEPT_EXTRA_VALUES)
+            return $values;
+
+        if ($strategy === self::STRATEGY_REFUSE_EXTRA_VALUES) {
+            foreach ($diff_keys as $key) {
+                $exception->addMessage(
+                    sprintf(
+                        "Extra field '%s' is present with STRATEGY_REFUSE_EXTRA_VALUES.",
+                        $key
+                    )
+                );
+            }
+
+            return $values;
+        }
+
+        if ($strategy === self::STRATEGY_IGNORE_EXTRA_VALUES) {
+            return array_diff_key($values, array_flip($diff_keys));
+        }
+
+        throw new \RuntimeException(
+            sprintf(
+                "Unknown strategy %d, available strategies are [STRATEGY_IGNORE_EXTRA_VALUES, STRATEGY_ACCEPT_EXTRA_VALUES, STRATEGY_REFUSE_EXTRA_VALUES].",
+                $strategy
+            )
+        );
+    }
+
+    /**
+     * checkFieldExists
+     *
+     * Throw an exception if the field does not exist.
+     *
+     * @throws  \InvalidArgumentException
+     */
+    private function checkFieldExists(string $name): self
+    {
+        if (!isset($this->fields[$name])) {
+            throw new \InvalidArgumentException(
+                sprintf(
+                    "Field '%s' is not declared, fields are {%s}.",
+                    $name,
+                    join(', ', array_keys($this->fields))
+                )
+            );
+        }
+
+        return $this;
+    }
+}
+
---
+++ sources/tests/Unit/ParameterJuicer.php
@@ -0,0 +1,162 @@
+<?php
+/*
+ * This file is part of Chanmix51’s ParameterJuicer package.
+ *
+ * (c) 2017 Grégoire HUBERT <hubert.greg@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Chanmix51\ParameterJuicer\Tests\Unit;
+
+use Chanmix51\ParameterJuicer\ValidationException;
+
+use \Atoum;
+
+/**
+ * ParameterJuicer
+ *
+ * ParameterJuicer test class
+ *
+ * @package     ParameterJuicer
+ * @copyright   2017 Grégoire HUBERT
+ * @author      Grégoire HUBERT <hubert.greg@gmail.com>
+ * @license     X11 {@link http://opensource.org/licenses/mit-license.php}
+ *
+ * @see         Atoum
+ */
+class ParameterJuicer extends Atoum
+{
+    /**
+     * testEmptyValidation
+     *
+     * Test empty validation works with STRATEGY_ACCEPT_EXTRA_VALUES.
+     */
+    public function testEmptyValidationWithAcceptStrategy()
+    {
+        $juicer = $this->newTestedInstance();
+        $this
+            ->assert("Testing an empty validator & values and STRATEGY_ACCEPT_EXTRA_VALUES")
+            ->given($data = [])
+                ->array($juicer->validateAndClean($data, 2))
+                    ->isEqualTo([])
+            ->assert("Testing an empty validator with values and STRATEGY_ACCEPT_EXTRA_VALUES")
+            ->given($data = ['pika' => 'chu'])
+                ->array($juicer->validateAndClean($data, 2))
+                    ->isEqualTo(['pika' => 'chu'])
+                    ;
+    }
+
+    /**
+     * testEmptyValidationWithIgnoreStrategy
+     *
+     * Empty validator with an ignore strategy returns empty sets.
+     */
+    public function testEmptyValidationWithIgnoreStrategy()
+    {
+        $juicer = $this->newTestedInstance();
+        $this
+            ->assert("Testing an empty validator and STRATEGY_IGNORE_EXTRA_VALUES.")
+            ->given($data = [])
+                ->array($juicer->validateAndClean($data, 0))
+                    ->isEqualTo([])
+            ->given($data = ['pika' => 'chu'])
+                ->array($juicer->validateAndClean($data, 0))
+                    ->isEqualTo([])
+            ;
+    }
+
+    /**
+     * testEmptyValidationWithRefuseStrategy
+     *
+     * This must refuse every values set but empty ones.
+     */
+    public function testEmptyValidationWithRefuseStrategy()
+    {
+        $juicer = $this->newTestedInstance();
+        $this
+            ->assert("Testing an empty validator & values and STRATEGY_REFUSE_EXTRA_VALUES.")
+            ->given($data = [])
+                ->array($juicer->validateAndClean($data, 1))
+                    ->isEqualTo([])
+            ->assert("Testing an empty validator with values and STRATEGY_REFUSE_EXTRA_VALUES.")
+            ->given($data = ['pika' => 'chu'])
+                ->exception(function() use ($juicer, $data) { return $juicer->validateAndClean($data, 1); })
+                    ->isInstanceOf('Chanmix51\ParameterJuicer\ValidationException')
+                    ;
+    }
+
+    public function butPikaOrChu(): array
+    {
+        return [
+            [[]],
+            [['pika' => 'yeaaaah']],
+            [['chu' => 'yeaaaah']],
+        ];
+    }
+
+    /**
+     * testMandatoryFieldsValidationFail
+     *
+     * Missing mandatory fields must fail validation.
+     * @dataProvider butPikaOrChu
+     */
+    public function testMandatoryFieldsValidationFail(array $data)
+    {
+        $juicer = $this->newTestedInstance()
+            ->addField('pika')
+            ->addValidator('pika', function($v) { })
+            ->addField('chu')
+            ;
+        $this
+            ->assert("Checking validation with missing mandatory data.")
+                ->exception(function() use ($juicer, $data) { return $juicer->validateAndClean($data, 2); })
+                    ->isInstanceOf('Chanmix51\ParameterJuicer\ValidationException')
+                    ->message->contains("Missing field")
+            ;
+    }
+
+    public function testMandatoryFieldValidation()
+    {
+        $validate_int = function($v) {
+            if (!is_int($v)) throw new ValidationException(
+                sprintf(
+                    "Field must be an integer ('%s' detected).",
+                    gettype($v)
+                )
+            );
+        };
+
+        $validate_range = function($v) {
+            if (10 < $v || 1 > $v) throw new ValidationException(
+                sprintf(
+                    "%d must be between 1 and 10.",
+                    $v
+                )
+            );
+        };
+        $juicer = $this->newTestedInstance()
+            ->addField('pika')
+            ->addValidator('pika', $validate_int)
+            ->addValidator('pika', $validate_range)
+            ->addField('chu')
+            ->addField('not mandatory', false)
+            ;
+        $this
+            ->assert("Checking validation with all mandatory data.")
+            ->given($data = ['pika' => 9, 'chu' => 'there'])
+                ->array($juicer->validateAndClean($data, 1))
+                ->isEqualTo($data)
+            ->assert("Checking validation with some wrong mandatory data (1/2).")
+            ->given($data['pika'] = 19)
+                ->exception(function() use ($juicer, $data) { return $juicer->validateAndClean($data, 1); })
+                    ->isInstanceOf('Chanmix51\ParameterJuicer\ValidationException')
+                    ->message->contains('must be between 1 and 10')
+            ->assert("Checking validation with some wrong mandatory data (2/2).")
+            ->given($data['pika'] = 'chu')
+                ->exception(function() use ($juicer, $data) { return $juicer->validateAndClean($data, 1); })
+                    ->isInstanceOf('Chanmix51\ParameterJuicer\ValidationException')
+                    ->message->contains('Field must be an integer')
+            ;
+    }
+}
