93596b1a7ac7fc69757d5e498c93dcdbfc154a7e
Grégoire HUBERT <hubert.greg@gmail.com>
2017-06-12 15:49:07 +0200

    make tests psr2 compliant

--- sources/tests/Unit/ParameterJuicer.php
+++ sources/tests/Unit/ParameterJuicer.php
@@ -374,9 +374,12 @@ class ParameterJuicer extends Atoum
     {
         $this
             ->assert('Checking mixing embedded validation with custom validators.')
-            ->given($cleanString = function($str) { return trim($str); })
+            ->given($cleanString = function ($str) {
+                return trim($str);
+            })
             ->given($mustBeNonEmptyString = function ($str) {
-                return $str !== '' ? null : 'must not be empty nor blank'; })
+                return $str !== '' ? null : 'must not be empty nor blank';
+            })
             ->given(
                 $juicer = ($this->newTestedInstance())
                     ->addField('my_form')
@@ -386,14 +389,16 @@ class ParameterJuicer extends Atoum
                                 ->addValidator('pass', $mustBeNonEmptyString)
                             ->addField('repass')
                                 ->addCleaner('repass', $cleanString)
-                                ->addValidator('repass', $mustBeNonEmptyString)
-                            )
+                                ->addValidator('repass', $mustBeNonEmptyString))
                     ->addValidator('my_form', function ($values) {
-                    return $values['pass'] === $values['repass']
+                        return $values['pass'] === $values['repass']
                         ? null
-                        : 'pass & repass do not match'; }))
-            ->exception(function() use ($juicer) {
-                return $juicer->squash(['my_form' => ['pass' => 'pika', 'repass' => 'not pika']]);})
+                        : 'pass & repass do not match';
+                    })
+            )
+            ->exception(function () use ($juicer) {
+                return $juicer->squash(['my_form' => ['pass' => 'pika', 'repass' => 'not pika']]);
+            })
             ->isInstanceOf('\Chanmix51\ParameterJuicer\Exception\ValidationException')
             ;
     }
